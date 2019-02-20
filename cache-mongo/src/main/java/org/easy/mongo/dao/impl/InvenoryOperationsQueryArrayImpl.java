d4ba5fa367f891ce69600ec3b5905e66cff09d45
yu0917 <290789221@qq.com>
2019-02-21 00:08:40 +0800

    aa

--- cache-mongo/src/main/java/org/easy/mongo/dao/impl/InvenoryOperationsQueryArrayImpl.java
+++ cache-mongo/src/main/java/org/easy/mongo/dao/impl/InvenoryOperationsQueryArrayImpl.java
@@ -9,21 +9,4 @@ import org.springframework.data.mongodb.core.query.Query;
 import java.util.List;
 
 public class InvenoryOperationsQueryArrayImpl {
-    @Autowired
-    private MongoOperations mongoOperations;
-
-    public List<Inventory> queryArray() {
-
-        return null;
-    }
-
-    public List<Inventory> queryArrayElemMatch(String status, Integer qty) {
-        Query query = new Query();
-        query.addCriteria( new Criteria().orOperator(
-                Criteria.where("status").elemMatch(
-                        Criteria.where("day").is(day),
-                        Criteria.where("day").is(day)),
-                Criteria.where("qty").lt(qty)));
-        return mongoOperations.find(query, Inventory.class);
-    }
 }
